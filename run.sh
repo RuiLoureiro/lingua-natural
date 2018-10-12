ccf46e780479fd94ed769e3a05b26c2880d8a46b
Rui Pedro Campinhos Loureiro <ruipcloureiro@gmail.com>
2018-10-12 14:03:07 +0100

    add dia

--- run.sh
+++ run.sh
@@ -10,10 +10,14 @@ fstconcat  date2date.fst mmm2mm.fst | fstclosure | fstdisambiguate > misto2numer
 
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico.fst | dot -Tpdf  > misto2numerico.pdf
 
+fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  dia.txt > dia.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait dia.fst | dot -Tpdf  > dia.pdf
+ 
 
-sudo python3 ./scripts/word2fst.py -s syms.txt 10/SET/2018 > dummy_input_date.txt
+python3 ./scripts/word2fst.py -s syms.txt 10/SET/2018 > dummy_input_date.txt
 fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  dummy_input_date.txt |  fstarcsort > dummy_input_date.fst
 
 fstcompose dummy_input_date.fst misto2numerico.fst > new_date.fst
 echo -n "New date é: "
 fstproject --project_output new_date.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
+
